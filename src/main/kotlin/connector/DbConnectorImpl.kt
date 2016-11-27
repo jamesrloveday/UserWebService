5ff467fbb0e62cadc492ff94f240797de1ab58cd
jamesrloveday <jamesrloveday@outlook.com>
2016-11-27 17:30:35 +0000

    Refactored the connector class
    Refactored the connector class to clean up and optimise the code for
    performance.

--- src/main/kotlin/connector/DbConnectorImpl.kt
+++ src/main/kotlin/connector/DbConnectorImpl.kt
@@ -1,6 +1,8 @@
 package connector
 
 import com.mongodb.MongoClient
+import com.mongodb.client.result.DeleteResult
+import com.mongodb.client.result.UpdateResult
 import objects.User
 import org.litote.kmongo.KMongo
 import org.litote.kmongo.*
@@ -11,6 +13,9 @@ interface DbConnector {
 
     fun addNewUser(user: User): String
     fun findUser(id: String): User
+    fun findAllUsers(): List<User>
+    fun updateUser(user: User): UpdateResult
+    fun delete(user: User): DeleteResult
 
 }
 
@@ -19,8 +24,7 @@ class DbConnectorImpl : DbConnector {
     val USER_STORE = "user_store"
 
     override fun  addNewUser(user: User): String {
-        val collection = getClient().getDatabase(USER_STORE).getCollection<User>()
-        collection.insertOne(user)
+        getClient().getDatabase(USER_STORE).getCollection<User>().insertOne(user)
         return user._id
     }
 
@@ -28,6 +32,15 @@ class DbConnectorImpl : DbConnector {
         val collection = getClient().getDatabase(USER_STORE).getCollection<User>()
         return collection.findOne("{_id: ${id.json}") ?: throw NullPointerException("User not found, try another id")
     }
+
+    override fun findAllUsers(): List<User> =
+            getClient().getDatabase(USER_STORE).getCollection<User>().find().toMutableList()
+
+    override fun updateUser(user: User): UpdateResult =
+            getClient().getDatabase(USER_STORE).getCollection<User>().updateOne(user)
+
+    override fun delete(user: User): DeleteResult =
+            getClient().getDatabase(USER_STORE).getCollection<User>().deleteOneById(user._id)
 
 }
 
